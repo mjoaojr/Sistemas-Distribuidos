e4f3f88a74346aba5e015206c0e55c17647546f6
mjoaojr <mjoaojr@gmail.com>
2018-03-15 11:10:35 -0300

    Criação de Threads
    Exemplo mais interessante de criação de Threads

---
+++ thr_hello2.py
@@ -0,0 +1,12 @@
+from threading import Thread
+
+def PrintHello(tid):
+	for i in range (5):
+		print ("Ola Mundo! Sou a thread "+str(tid))
+
+
+
+for i in range (5):
+	print ("Criando thread "+str(i))
+	t = Thread(target=PrintHello,args=(i,))
+	t.start()
