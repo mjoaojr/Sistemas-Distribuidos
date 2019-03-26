64008c24dc3239a5217f8e9666ead2ac0b590017
Mario Joao Jr <mjoaojr@gmail.com>
2019-03-25 21:58:08 -0300

    Mensagem de Final da thread

--- thr_hello.py
+++ thr_hello.py
@@ -3,6 +3,7 @@ from threading import Thread
 def PrintHello(tid):
 	for i in range (5):
 		print ("Ola Mundo! Sou a thread "+str(tid)+" - "+str(i))
+	print ("Thread %d: morri"%(tid))
 
 
 
@@ -10,3 +11,4 @@ for i in range (5):
 	print ("Criando thread "+str(i))
 	t = Thread(target=PrintHello,args=(i,))
 	t.start()
+print ("Thread Mae: morri")
